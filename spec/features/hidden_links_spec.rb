cdc02c553a1383cd9f189b11307e0d1df9661cc3
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2018-08-07 14:03:45 -0500

    fixing ticket deletion and ticket viewing

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -1,14 +1,19 @@
-context "non-admin users (project viewers)" do 
-    before do 
-        login_as(user)
-        assign_role!(user, :viewer, project)
-    end
+require "rails_helper" 
+RSpec.feature "Users can view projects" do
+    let(:user) { FactoryGirl.create(:user)}
+    let(:project) {FactoryGirl.create(:project, name: "Sublime Text 3")}
+    context "non-admin users (project viewers)" do 
+        before do 
+            login_as(user)
+            assign_role!(user, :viewer, project)
+        end
 
-    scenario "cannot see the Edit Project link" do 
-        visit project_path(project)
-        expect(page).not_to have_link "Edit Project"
-    end
-end 
+        scenario "cannot see the Edit Project link" do 
+            visit project_path(project)
+            expect(page).not_to have_link "Edit Project"
+        end
+    end 
+end
 
 context "admin users" do 
         scenario "can see the Edit Project link" do 
