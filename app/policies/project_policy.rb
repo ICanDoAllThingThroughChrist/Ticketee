a1376221c77ef38fea8f7e6356436ab4499a6fed
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2018-08-07 12:03:56 -0500

    don't list projects that a user doesn't have permission to see

--- app/policies/project_policy.rb
+++ app/policies/project_policy.rb
@@ -1,9 +1,12 @@
 class ProjectPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope
+    class Scope < Scope
+      def resolve
+        return scope.none if user.nil?
+        return scope.all if user.admin? 
+
+        scope.joins(:roles).where(roles: {user_id: user})
+      end
     end
-  end
 
   def show? 
     user.try(:admin?) || record.roles.exists?(user_id: user)
--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -1,6 +1,18 @@
-context "non-admin users (projec viewers)" do 
+context "non-admin users (project viewers)" do 
     before do 
         login_as(user)
         assign_role!(user, :viewer, project)
     end
+
+    scenario "cannot see the Edit Project link" do 
+        visit project_path(project)
+        expect(page).not_to have_link "Edit Project"
+    end
+end 
+
+context "admin users" do 
+        scenario "can see the Edit Project link" do 
+            visit project_path(project)
+            expect(page).to have_link "Edit Project"
+        end 
 end 
